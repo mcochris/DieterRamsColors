303b42891770036a9e91c420348bd5091b7ab210
Chris Strawser <mcochris@gmail.com>
2023-10-01 09:45:20 -0400

    Huh?

--- js/setInitialColorTheme.js
+++ js/setInitialColorTheme.js
@@ -1,14 +1,14 @@
 {
-    const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
-    const themePickerButtons = document.querySelectorAll(".theme-picker button");
-    if (darkThemeMq.matches) {
-        document.body.classList.add("dark-theme");
-        themePickerButtons[1].style.textDecoration = "underline";
-    }
-    else {
-        document.body.classList.add("light-theme");
-        themePickerButtons[0].style.textDecoration = "underline";
-    }
+	const darkThemeMq = window.matchMedia("(prefers-color-scheme: dark)");
+	const themePickerButtons = document.querySelectorAll(".theme-picker button");
+	if (darkThemeMq.matches) {
+		document.body.classList.add("dark-theme");
+		themePickerButtons[1].style.textDecoration = "underline";
+	}
+	else {
+		document.body.classList.add("light-theme");
+		themePickerButtons[0].style.textDecoration = "underline";
+	}
 }
-export {};
+export { };
 //# sourceMappingURL=setInitialColorTheme.js.map
